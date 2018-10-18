38b797d6f08a3be702b62cfbef86e7140f1263fd
uferesamuel <uferesamuel@gmail.com>
2018-10-18 13:35:22 +0100

    authentication cimplete

--- app/src/main/java/com/example/ighub/contact_app/MainActivity.java
+++ app/src/main/java/com/example/ighub/contact_app/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.ighub.contact_app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -7,6 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etFirstname, etLastname, etEmail, etPhone, etPassword, etConfrimPass;
@@ -35,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         phone = etPhone.getText().toString().trim();
         password = etPassword.getText().toString().trim();
         confirmpass = etConfrimPass.getText().toString().trim();
-//        gender = spGender.getSelectedItem(spGender.getSelectedItemId());
+        gender = spGender.getItemAtPosition(spGender.getSelectedItemPosition()).toString();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,11 +54,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void validation() {
-        if (TextUtils.isEmpty(fullname)) {
-            etFirstname.setError("enter first name");
-            etLastname.setError("enter last name");
-        } else if (TextUtils.isEmpty(email)) {
-            etEmail.setError("enter your email");
+        if (isNetworkConnectionAvailable()) {
+            //do something
+            if (TextUtils.isEmpty(firstName)) {
+                etFirstname.setError("First name cannot be empty");
+                return;
+            } if (TextUtils.isEmpty(lastName)) {
+                etLastname.setError("Last name cannot be empty");
+                return;
+            } if (isValidEmaillId(email)) {
+                etEmail.setError("Please enter a correct email");
+                return;
+            } if (password.length() < 6) {
+                etPassword.setError("Password cannot be less than 6");
+                return;
+            } if (confirmpass != password) {
+                etConfrimPass.setError("Password does not match");
+                return;
+            } if (!phone.startsWith("0")) {
+                etPhone.setError("Invalid phone number");
+                return;
+            } if (phone.length() != 11 ) {
+                etPhone.setError("Phone number cannot be less than 11");
+            } else {
+                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, Welcome.class));
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Please put on your data connection",
+                    Toast.LENGTH_LONG).show();
         }
+    }
+
+    // added as an instance method to an Activity
+    boolean isNetworkConnectionAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null) return false;
+        NetworkInfo.State network = info.getState();
+        return (network == NetworkInfo.State.CONNECTED || network == NetworkInfo.State.CONNECTING);
+    }
+
+    boolean isValidEmaillId(String email){
+        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
     }
 }
